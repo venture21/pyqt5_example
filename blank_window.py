8363755691739ea91bb0cc6947781197fc7a194d
venture21 <park0122@gmail.com>
2025-07-01 16:25:11 +0900

    주석을 추가

--- blank_window.py
+++ blank_window.py
@@ -3,17 +3,22 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
-
+# QWidget이 부모 클래스
+# 부모 클래스를 상속 받아서 MyApp이라는 클래스를 생성
 class MyApp(QWidget):
 
     def __init__(self):
+        # super는 부모 클래스를 가리킨다.
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('My First Application')
-        self.move(300, 300)
-        self.resize(400, 200)
+        # 창의 이름을 설정
+        self.setWindowTitle('MyApp')
+        # 창을 띄우는  위치
+        self.move(1000, 100)
+        #
+        self.resize(400, 400)
         self.show()
 
 
